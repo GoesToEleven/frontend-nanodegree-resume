54453c9134e94d328df5603c872f576d08c59f6a
Todd McLeod <todd.mcleod@fresnocitycollege.edu>
2014-11-11 07:15:01 -0800

    updates name role on resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,29 +1,10 @@
-/*
-* VANILLA JS
-*/
-var stringToAdd = "";
+
 var name = "Todd McLeod";
-stringToAdd += "<p>" + name + "</p>";
-
-var email = "toddmcleod@udacity.com";
-var newEmail = email.replace("udacity", "gmail");
-console.log("email: " + email);
-console.log("new email: " + newEmail);
-stringToAdd += ("<p>email: " + email + "</p>");
-stringToAdd += ("<p>new email: " + newEmail + "</p>");
-
-var awesomeThoughts = "My name is Todd and I am awesome!";
-var funThoughts = awesomeThoughts.replace("awesome!", "FUN");
-stringToAdd += ("<p>" + awesomeThoughts + "</p>");
-
-document.querySelector("#main").innerHTML = stringToAdd;
-
+var role = "Web Developer";
 var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
 
-document.querySelector("#header").append(formattedName);
+// build header
+var stringToInsert = formattedName + " " + formattedRole;
 
-/*
- * JQUERY
- */
-
-//$("#main").append("Todd");
+document.querySelector("#header").innerHTML = stringToInsert;
