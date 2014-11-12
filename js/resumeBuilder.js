0de4ada905f93bc95cf12debc357d41c7bd328ab
Todd McLeod <todd.mcleod@fresnocitycollege.edu>
2014-11-12 10:23:00 -0800

    changes code to match solution

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,28 +1,29 @@
 
 var bio = {
-    "name" : "Todd McLeod",
-    "role" : "Web Developer",
-    "contact" : "Contact",
-    "contact2" : "contact2",
-    "mobile" : "559-744-3647",
-    "email" : "toddmcleod@gmail.com",
-    "twitter" : "https://twitter.com/Todd_McLeod",
-    "github" : "https://github.com/GoesToEleven",
-    "location" : "Fresno, CA",
-    "pictureURL" : "images/me.jpg",
-    "welcomeMessage" : "Beautiful design, elegant code",
-    "skills" : "HTML, CSS, JavaScript"
+    "name": "Todd McLeod",
+    "role": "Web Developer",
+    "contacts": {
+        "mobile": "559-744-3647",
+        "email": "toddmcleod@gmail.com",
+        "twitter": "@Todd_McLeod",
+        "github": "GoesToEleven",
+        "location": "California"
+    },
+    "welcomeMessage": "Beautiful design, elegant code",
+    "skills": "HTML, CSS, JavaScript",
+    //"skills": ["HTML", "CSS", "JavaScript"],
+    "pictureURL": "images/me.jpg"
 };
 
 var formattedName = HTMLheaderName.replace("%data%", bio.name);
 var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
 //var formattedContact = HTMLcontactGeneric.replace("%contact%", bio.contact);
 //formattedContact = formattedContact.replace("%data%", bio.contact2);
-var formattedMobile = HTMLmobile.replace("%data%", bio.mobile);
-var formattedEmail = HTMLemail.replace("%data%", bio.email);
-var formattedTwitter = HTMLtwitter.replace("%data%", bio.twitter);
-var formattedGithub = HTMLgithub.replace("%data%", bio.github);
-//var formattedLocation = HTMLlocation.replace("%data%", bio.location);
+var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
 var formattedPictureURL = HTMLbioPic.replace("%data%", bio.pictureURL);
 var formattedWelcomeMessage = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage);
 var formattedSkills = HTMLskills.replace("%data%", bio.skills);
@@ -34,7 +35,7 @@ document.querySelector("#header ul").insertAdjacentHTML("beforeend", formattedMo
 document.querySelector("#header ul").insertAdjacentHTML("beforeend", formattedEmail);
 document.querySelector("#header ul").insertAdjacentHTML("beforeend", formattedTwitter);
 document.querySelector("#header ul").insertAdjacentHTML("beforeend", formattedGithub);
-//document.querySelector("#header ul").insertAdjacentHTML("beforeend", formattedLocation);
+document.querySelector("#header ul").insertAdjacentHTML("beforeend", formattedLocation);
 document.querySelector("#header ul").insertAdjacentHTML("afterend", formattedPictureURL);
 document.querySelector("#header ul").insertAdjacentHTML("afterend", formattedWelcomeMessage);
 document.querySelector("#header ul:last-child").insertAdjacentHTML("beforeend", formattedSkills);
