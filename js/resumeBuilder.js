a6892013914ba8410c37093ed28565e9970db704
Todd McLeod <todd.mcleod@fresnocitycollege.edu>
2014-11-12 08:13:42 -0800

    before adding object literal notation

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,8 +3,19 @@ var name = "Todd McLeod";
 var role = "Web Developer";
 var formattedName = HTMLheaderName.replace("%data%", name);
 var formattedRole = HTMLheaderRole.replace("%data%", role);
+document.querySelector("#header").insertAdjacentHTML("afterbegin", formattedRole);
+document.querySelector("#header").insertAdjacentHTML("afterbegin", formattedName);
+
+var bio = {
+    "name" : "Todd McLeod",
+    "role" : "Web Developer",
+    "contactInfo" : "toddmcleod@gmail.com",
+    "pictureURL" : "images/me.jpg",
+    "welcomeMessage" : "Beautiful design, elegant code",
+    "skills" : "HTML, CSS, JavaScript"
+};
+
 
 // build header
-var stringToInsert = formattedName + " " + formattedRole;
-
-document.querySelector("#header").innerHTML = stringToInsert;
+//var stringToInsert = formattedName + " " + formattedRole;
+//document.querySelector("#header").insertAdjacentHTML("afterbegin", stringToInsert);
--- lesson_01/string_manip_names.js
+++ lesson_01/string_manip_names.js
@@ -1,3 +1,25 @@
-/**
- * Created by tm on 11/11/2014.
- */
+var name = "AlbERt EINstEiN";
+
+function nameChanger(oldName) {
+    var finalName = oldName;
+    // Your code goes here!
+    var breakBetweenNames = finalName.indexOf(" ");
+    var firstName = (finalName.slice(0,breakBetweenNames)).toLowerCase();
+    firstName = firstName[0].toUpperCase() + firstName.slice(1);
+    var lastName = finalName.slice(breakBetweenNames+1).toUpperCase();
+    finalName = firstName + " " + lastName;
+    // Don't delete this line!
+    return finalName;
+}
+
+//function nameChanger(oldName) {
+//    var finalName = oldName;
+//    var names = oldName.split(" ");
+//    names[1] = names[1].toUpperCase();
+//    names[0] = names[0].slice(0,1).toUpperCase() + names[0].slice(1).toLowerCase();
+//    finalName = names.join(" ");
+//    return finalName;
+//}
+
+// Did your code work? The line below will tell you!
+console.log(nameChanger(name));
